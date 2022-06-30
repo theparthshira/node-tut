91a832eb86ad22008369ab8857d6a99af0218dbe
theparthshira <shirawalaparth@gmail.com>
2022-06-30 19:49:37 +0530

    again after long time

--- mem-8.js
+++ mem-8.js
@@ -1,18 +1,18 @@
-const http = require('http')
+const http = require("http");
 
-const server = http.createServer((req,res)=>{
-    if(req.url === '/') {
-        res.end('Hello World')
-    }
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("Hello World");
+  }
 
-    if(req.url === '/about') {
-        res.end('Hello about')
-    }
+  if (req.url === "/about") {
+    res.end("Hello about");
+  }
 
-    res.end(`
+  res.end(`
         <h1>Oops</h1>
         <a href="/"> homepage </a>
-    `)
-})
+    `);
+});
 
-server.listen(5000)
+server.listen(5000);
--- names.js
+++ names.js
@@ -2,6 +2,6 @@ const pratham='pratham'
 const swapnil='swapnil'
 const parth='parth'
 
-module.exports={swapnil,pratham};
+module.exports={swapnil,pratham,parth};
 
 // console.log(module);
